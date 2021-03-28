8f34d842377522c4afa76c8513ab58c965e8a046
Marius Tendeland Horne <marius_t_h@hotmail.com>
2021-03-28 21:38:40 +0200

    Kinda working test

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
 <template>
-  <div id="app">
-    <Page />
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="logo.png"
+          transition="scale-transition"
+          width="60"
+        />
+
+        Div oppskrifter
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <Page/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Page from './components/Page.vue'
+import Page from './components/Page';
 
 export default {
   name: 'App',
-  components: {
-    Page
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Page,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,52 +1,84 @@
 <template>
-  <div class="hello">
-    asd
-  </div>
+	<div>
+		<v-tabs>
+			<v-tab
+				v-for="kategori in kategorier"
+				:key="kategori"
+				v-on:click="select(kategori)"
+				>{{ kategori }}</v-tab
+			>
+		</v-tabs>
+		<div v-if="selectedKategori != ''">
+			<v-tabs>
+				<v-tab
+					v-for="(oppskrift, i) in oppskrifter[selectedKategori]"
+					:key="i"
+					v-on:click="selectOppskrift(oppskrift)"
+					>{{ oppskrift.navn }}</v-tab
+				>
+			</v-tabs>
+		</div>
+		<div v-if="selectedOppskrift">
+			<div>
+        <u>Ingredienser</u><br>
+        <span v-html="selectedOppskrift.ingredienser"></span>
+			</div>
+      <br>
+			<div>
+        <u>Steg</u><br>
+        <span v-html="selectedOppskrift.steg"></span>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import { db } from "../firebase.js";
 
 export default {
-  name: 'HelloWorld',
-  data: function (){
-    return {
-      oppskrifter: {},
-      kategorier: []
-    }
-  },
-  mounted: async function () {
-    const oppskrifter = await db.collection('oppskrifter').get();
-    oppskrifter.docs.forEach(async (doc) => {
-      const data = doc.data();
-      const kategori = data.Kategori;
-      const ingredienser = data.ingredienser;
-      const steg = data.steg;
-      this.kategorier.push(kategori);
-      this.oppskrifter[kategori] = {
-        "ingredienser": ingredienser,
-        "steg": steg
-      }
-    });
-  }
-}
+	name: "MainPage",
+	data: function() {
+		return {
+			oppskrifter: {},
+			kategorier: [],
+			selectedKategori: "",
+			selectedOppskrift: undefined,
+		};
+	},
+	methods: {
+		select: function(kategori) {
+			this.selectedKategori = kategori;
+      // Setting it back to undefined to avoid showing another recipe while looking for another.
+      this.selectedOppskrift = undefined;
+			console.log(this.oppskrifter);
+		},
+		selectOppskrift: function(oppskrift) {
+			this.selectedOppskrift = oppskrift;
+			console.log(oppskrift);
+		},
+	},
+	mounted: async function() {
+		const oppskrifter = await db.collection("oppskrifter").get();
+		oppskrifter.docs.forEach(async (doc) => {
+			const data = doc.data();
+			const kategori = data.Kategori;
+			const ingredienser = data.ingredienser;
+			const navn = data.Navn;
+			const steg = data.steg;
+			this.kategorier.push(kategori);
+			if (!this.oppskrifter[kategori]) {
+				this.oppskrifter[kategori] = {};
+			}
+			this.oppskrifter[kategori][navn] = {
+				navn: navn,
+				ingredienser: ingredienser,
+				steg: steg,
+			};
+		});
+	},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-div {
-  margin: 40px 0 0;
-  background-color: blueviolet;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
